0755992b7a3852cfcec656eee00ea986fc250fcb
Adrian Brown <adrian-brown@mail.com>
2017-04-01 14:43:25 +0100

    initial commit for DoorBird plugin

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # homebridge-doorbird
-Homebridge plugin for DoorBird 
+
+`homebridge-doorbird` is a plugin for Homebridge.  Giving you a basic experience with your [DoorBird](https://www.doorbird.com) unit.
+
+It connects a motion sensor to your DoorBird unit, acting on doorbell activity.  You have to add the motion sensor to the same room as the camera stream (use ffmpeg plugin) and activate notfications on the sensor if you want iOS notifications pushed to your Home screen, limited to users in WIFI range only, and requires a Hub e.g. Apple TV or iPad.  
+
+## Installation
+
+If you are new to Homebridge, please first read the Homebridge [documentation](https://www.npmjs.com/package/homebridge).
+
+1 Install homebridge:
+```sh
+sudo npm install -g homebridge
+```
+2 Install homebridge-doorbird:
+```sh
+sudo npm install -g git+https://github.com/brownad/homebridge-doorbird.git
+```
+3 Configure plugin:
+```sh
+ Update your configuration file. See config.json in this repository for a sample. 
+```
+If you want it to trigger off the motion sensor, swap the `doorbell` for `motionsensor`.  Or have them both.
+
+## Configuration
+
+Add the platform in [`config.json`](https://github.com/brownad/homebridge-doorbird/blob/master/config.json) in your home directory inside `.homebridge`.  
+
+## Note
+If placed in a room with a camera it sends a notification with a snapshot/stream to your iOS device(s) after user pushes the DoorBird's doorbell button.
+
+## Credits
+https://github.com/Samfox2/homebridge-doorbell
---
+++ index.js
@@ -0,0 +1,77 @@
+var Service, Characteristic;
+var request = require('request');
+var hyperquest = require('hyperquest');
+
+module.exports = function(homebridge) {
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  homebridge.registerPlatform("homebridge-doorbird", "Doorbell", DoorBirdPlatform);
+};
+
+function DoorBirdPlatform(log, config) {
+  this.log = log;
+  this.devices = config["doorbells"];
+  log("Starting discovery...");
+}
+
+DoorBirdPlatform.prototype = {
+  accessories: function(callback) {
+    var foundAccessories = [];
+    var count = this.devices.length;
+
+    for(index = 0; index < count; index++){
+		  var accessory  = new DoorBirdAccessory(this.log, this.devices[index]);
+		  foundAccessories.push(accessory);
+	  }
+      callback(foundAccessories);
+  }
+};
+
+function DoorBirdAccessory(log, config) {
+  var self = this;
+  this.log = log;
+  this.name = config["name"];
+  this.username = config["doorbird_username"];
+  this.password = config["doorbird_password"];
+  this.ip = config["doorbird_ip"];
+  this.url = config["doorbird_url"];
+  this.binaryState = 0; 
+  this.log("Starting a homebridge-doorbird device with name '" + this.name + "'...");
+  this.service;
+  this.timeout = 2;
+  var url = "http://" + this.ip + this.url + "&http-user=" + this.username + "&http-password=" + this.password 
+ 
+  var r = hyperquest(url)
+  r.on('data', function(response) {
+    var doorbirdResponse = String(response)
+    var doorbellState = doorbirdResponse.split(/[:]+/).pop();
+    if(doorbellState.trim() != "L") {
+      console.log("DoorBird doorbell pressed");
+      setTimeout(function() {
+        self.log("DoorBird doorbell pressed");
+ 	self.service.getCharacteristic(Characteristic.MotionDetected).updateValue(true);
+ 	}.bind(self), 10);
+      
+      //reset state
+      setTimeout(function() {
+        console.log("Resetting Doorbird")
+        self.service.getCharacteristic(Characteristic.MotionDetected).updateValue(false);
+        }.bind(self), 5000); 
+      };  
+    })
+   };	  
+
+DoorBirdAccessory.prototype.getServices = function() {
+    var informationService = new Service.AccessoryInformation();
+    informationService
+      .setCharacteristic(Characteristic.Manufacturer, "DoorBird")
+      .setCharacteristic(Characteristic.Model, "D101")
+      .setCharacteristic(Characteristic.SerialNumber, "4260423860001");
+	
+    this.service = new Service.MotionSensor(this.name);
+    
+    var targetChar = this.service
+      .getCharacteristic(Characteristic.MotionDetected);
+
+return [this.service, informationService];
+};
